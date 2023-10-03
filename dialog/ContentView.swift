983f2af76cd3a65d731b7d76d885e3ccb03c0693
emioiso <jijjjiijiijijiji@gmail.com>
2023-10-03 23:55:22 +0900

    .gitignoreとdialogを作ったよ

--- dialog/ContentView.swift
+++ dialog/ContentView.swift
@@ -1,6 +1,6 @@
 //
 //  ContentView.swift
-//  dialog
+//  alert
 //
 //  Created by emi oiso on 2023/10/03.
 //
@@ -8,14 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isShowAlert = false
+    @State var isShowDialog = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button("アラート"){
+                isShowAlert = true
+            }
+            .padding()
+            
+            Button("ダイアログ"){
+                isShowDialog = true
+            }
+            .padding()
         }
         .padding()
+        .alert("本当に削除しますか？",isPresented: $isShowAlert){
+            Button("削除する",role: .destructive){
+                
+            }
+            Button("キャンセル",role: .cancel){
+                
+            }
+        } message: {
+            Text("一度削除したら元に戻すことはできません")
+        }
+        .confirmationDialog("タイトル", isPresented: $isShowDialog,titleVisibility: .visible) {
+            Button("選択肢1"){
+                
+            }
+            Button("選択肢2"){
+                
+            }
+            Button("選択肢3"){
+                
+            }
+            Button("キャンセル",role: .cancel){
+                
+            }
+        } message: {
+            Text("ここにメッセージ")
+        }
     }
 }
 
